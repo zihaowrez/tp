8065477fae3c25b9cd3721281eb8cc796c14721a
chriswengqi <chris_wengqi@hotmail.com>
2022-03-11 22:09:23 +0800

    Implement ViewCommand to view specific contact details

---
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Views a particular contact "
+            + "given the specified index and displays the details of the contact at said index.\n"
+            + "Parameters: INDEX...\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    private final Index index;
+
+    public ViewCommand(Index index) {
+        this.index = index;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+        Person viewPerson = lastShownList.get(index.getZeroBased());
+        model.updateContactDetails(new Predicate<Person>() {
+            @Override
+            public boolean test(Person p) {
+                return p.equals(viewPerson);
+            }
+        });
+
+        return new CommandResult(String.format(Messages.MESSAGE_CONTACT_DETAILS, viewPerson.getName()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ViewCommand // instanceof handles nulls
+                && index.equals(((ViewCommand) other).index)); // state check
+    }
+}
