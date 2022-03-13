5bdddf3ebc81ab7028ac277ac4c2b18d65b902c4
zihaowrez <wuzihaohy@gmail.com>
2022-03-13 12:47:56 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,11 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.IndexedDeleteCommand;
 import seedu.address.logic.commands.NamedDeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
