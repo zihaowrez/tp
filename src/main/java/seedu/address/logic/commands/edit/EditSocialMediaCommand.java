c8d3af94557fb234e0d53a4a8b19b88e74816491
Wong Chee Hong <c.h.wong2606@gmail.com>
2022-03-30 02:32:56 +0800

    Add command and parser for editing social media

---
+++ src/main/java/seedu/address/logic/commands/edit/EditSocialMediaCommand.java
@@ -0,0 +1,85 @@
+package seedu.address.logic.commands.edit;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PLATFORM_NAME_FLAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SOCIAL_MEDIA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.delete.Target;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.socialmedia.PlatformDescription;
+import seedu.address.model.socialmedia.PlatformName;
+import seedu.address.model.socialmedia.SocialMedia;
+
+public class EditSocialMediaCommand extends EditCommand {
+    
+    public static final String MESSAGE_EDIT_SOCIALS_SUCCESS = "Edited social media of %s: From %s to %s";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the targeted social media of a given person. "
+            + "The social media is specified by the index of list of socials of the given person.\n"
+            + "Parameters:"
+            + "PERSON_NAME/INDEX"
+            + PREFIX_INDEX + "INDEX_NUM"
+            + "[" + PREFIX_PLATFORM_NAME_FLAG + "]"
+            + PREFIX_SOCIAL_MEDIA + "UPDATED_VALUE";
+
+    private Target target;
+    private Index index;
+    private String newDetails;
+    private boolean editPlatformNameflag;
+    
+    public EditSocialMediaCommand(Object target, Index index, String newDetails, boolean editPlatformNameflag) {
+        assert target instanceof Name || target instanceof Index;
+        
+        if (target instanceof Name) {
+            this.target = Target.of((Name) target, null);
+        } else if (target instanceof Index) {
+            this.target = Target.of((Index) target, null);
+        } else {
+            this.target = null;
+        }
+
+        this.index = index;
+        this.newDetails = newDetails;
+        this.editPlatformNameflag = editPlatformNameflag;
+
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getSortedAndFilteredPersonList();
+        target.setTargetList(lastShownList);
+
+        Person targetPersonToEdit = target.targetPerson();
+        List<SocialMedia> socialsToEdit = new ArrayList<>(targetPersonToEdit.getSocialMedias());
+        SocialMedia socialMediaToEdit = socialsToEdit.get(index.getZeroBased());
+        SocialMedia updatedSocialMedia; 
+
+        if (editPlatformNameflag) {
+            updatedSocialMedia = new SocialMedia(new PlatformName(newDetails), socialMediaToEdit.getPlatformDescription());
+        } else {
+            updatedSocialMedia = new SocialMedia(socialMediaToEdit.getPlatformName(), new PlatformDescription(newDetails));
+        }
+
+        socialsToEdit.set(index.getZeroBased(), updatedSocialMedia);
+        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+        editPersonDescriptor.setSocials(socialsToEdit);
+
+        Person updatedPerson = EditPersonDescriptor.createEditedPerson(targetPersonToEdit, editPersonDescriptor);
+        model.setPerson(targetPersonToEdit, updatedPerson);
+        model.updateSelectedPerson(updatedPerson);
+        return new CommandResult(String.format(MESSAGE_EDIT_SOCIALS_SUCCESS, 
+                updatedPerson.getName(), socialMediaToEdit, updatedSocialMedia));
+    }
+
+    
+}
---
+++ src/main/java/seedu/address/logic/parser/EditSocialMediaCommandParser.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PLATFORM_NAME_FLAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SOCIAL_MEDIA;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.edit.EditSocialMediaCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class EditSocialMediaCommandParser implements Parser<EditSocialMediaCommand> {
+
+    @Override
+    public EditSocialMediaCommand parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(userInput,
+                PREFIX_SOCIAL_MEDIA, PREFIX_PLATFORM_NAME_FLAG, PREFIX_INDEX);
+
+        Object target;
+
+        try {
+            target = ParserUtil.parseTarget(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditSocialMediaCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (!argMultimap.arePrefixesPresent(PREFIX_INDEX, PREFIX_SOCIAL_MEDIA)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditSocialMediaCommand.MESSAGE_USAGE));
+        }
+
+        String newSocialMedia = argMultimap.getValue(PREFIX_SOCIAL_MEDIA).get();
+        Index oneBasedIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_INDEX).get());
+
+        if (argMultimap.doesPrefixesExist(PREFIX_PLATFORM_NAME_FLAG)) {
+            return new EditSocialMediaCommand(target, oneBasedIndex, newSocialMedia, true);
+        } else { 
+            return new EditSocialMediaCommand(target, oneBasedIndex, newSocialMedia, false);
+        }
+        
+
+    }
+    
+}
