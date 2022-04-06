ac03230e1bcb6874fc4c306e0ec8ecb0e7511d53
Wong Chee Hong <c.h.wong2606@gmail.com>
2022-04-06 21:01:51 +0800

    Fix Email rejecting consecutive special chars
    Previously, emails such as peter__jack@example.com are considered
    invalid emails. However, emails with consecutive special chars do exist
    and are somewhat common.

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -12,7 +12,8 @@ public class Email {
     private static final String SPECIAL_CHARACTERS = "+_.-";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
-            + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
+            + "1. The local-part should only contain alphanumeric characters and these special "
+            + "characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
             + "characters.\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
@@ -22,12 +23,13 @@ public class Email {
             + "    - have each domain label start and end with alphanumeric characters\n"
             + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
     // alphanumeric and special characters
-    private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
-    private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
-            + ALPHANUMERIC_NO_UNDERSCORE + ")*";
+    private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]"; // alphanumeric characters except underscore
+    private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "(([[" + SPECIAL_CHARACTERS + "]"
+            + ALPHANUMERIC_NO_UNDERSCORE + "])*" + ALPHANUMERIC_NO_UNDERSCORE + ")?";
     private static final String DOMAIN_PART_REGEX = ALPHANUMERIC_NO_UNDERSCORE
-            + "(-" + ALPHANUMERIC_NO_UNDERSCORE + ")*";
-    private static final String DOMAIN_LAST_PART_REGEX = "(" + DOMAIN_PART_REGEX + "){2,}$"; // At least two chars
+            + "(" + "([-" + ALPHANUMERIC_NO_UNDERSCORE + "])*" + ALPHANUMERIC_NO_UNDERSCORE + ")?";
+    private static final String DOMAIN_LAST_PART_REGEX = ALPHANUMERIC_NO_UNDERSCORE + "([-"
+            + ALPHANUMERIC_NO_UNDERSCORE + "])*" + ALPHANUMERIC_NO_UNDERSCORE;
     private static final String DOMAIN_REGEX = "(" + DOMAIN_PART_REGEX + "\\.)*" + DOMAIN_LAST_PART_REGEX;
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@" + DOMAIN_REGEX;
 
--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -21,6 +21,7 @@ public class EmailTest {
 
     @Test
     public void isValidEmail() {
+
         // null email
         assertThrows(NullPointerException.class, () -> Email.isValidEmail(null));
 
@@ -44,7 +45,6 @@ public class EmailTest {
         assertFalse(Email.isValidEmail("peter@jack@example.com")); // '@' symbol in local part
         assertFalse(Email.isValidEmail("-peterjack@example.com")); // local part starts with a hyphen
         assertFalse(Email.isValidEmail("peterjack-@example.com")); // local part ends with a hyphen
-        assertFalse(Email.isValidEmail("peter..jack@example.com")); // local part has two consecutive periods
         assertFalse(Email.isValidEmail("peterjack@example@com")); // '@' symbol in domain name
         assertFalse(Email.isValidEmail("peterjack@.example.com")); // domain name starts with a period
         assertFalse(Email.isValidEmail("peterjack@example.com.")); // domain name ends with a period
@@ -57,12 +57,19 @@ public class EmailTest {
         assertTrue(Email.isValidEmail("PeterJack.1190@example.com")); // period in local part
         assertTrue(Email.isValidEmail("PeterJack+1190@example.com")); // '+' symbol in local part
         assertTrue(Email.isValidEmail("PeterJack-1190@example.com")); // hyphen in local part
+        assertTrue(Email.isValidEmail("peter..jack@example.com")); // local part has two consecutive periods
+        assertTrue(Email.isValidEmail("peter--jack@example.com")); // local part has two consecutive hypens
+        assertTrue(Email.isValidEmail("peter__jack@example.com")); // local part has two consecutive hypens
         assertTrue(Email.isValidEmail("a@bc")); // minimal
         assertTrue(Email.isValidEmail("test@localhost")); // alphabets only
         assertTrue(Email.isValidEmail("123@145")); // numeric local part and domain name
         assertTrue(Email.isValidEmail("a1+be.d@example1.com")); // mixture of alphanumeric and special characters
         assertTrue(Email.isValidEmail("peter_jack@very-very-very-long-example.com")); // long domain name
         assertTrue(Email.isValidEmail("if.you.dream.it_you.can.do.it@example.com")); // long local part
+        assertTrue(Email.isValidEmail("if_you_dream_it_you.can.do.it@example.com")); // many underscores
+        assertTrue(Email.isValidEmail("peter_jack@very--a--example.com")); // many hypens in domain
         assertTrue(Email.isValidEmail("e1234567@u.nus.edu")); // more than one period in domain
+        assertTrue(Email.isValidEmail("e1234567@u.n.u.s.edu")); // more than one 1-char domain-part
+        assertTrue(Email.isValidEmail("e1234567@u.n.u.s.ed")); // more than one 1-char domain- and 2 letter TLD
     }
 }
