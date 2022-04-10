114cbe0cfb9cdec57200e99daf503110a389db3e
kavxya <57605345+kavxya@users.noreply.github.com>
2022-04-10 17:48:58 +0800

    Update TagPanel.java
    Checkstyle

--- src/main/java/seedu/address/ui/TagPanel.java
+++ src/main/java/seedu/address/ui/TagPanel.java
@@ -8,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
